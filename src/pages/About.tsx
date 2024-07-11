761efe75e2cb07c253aada249471b97a2f639912
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-07-11 22:43:47 +0600

    route added

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nihil.
+      Alias quam voluptates blanditiis dolor molestiae, quidem nam velit
+      dignissimos corrupti ad cumque, minima inventore, enim accusantium eius
+      omnis provident?
+    </div>
+  );
+};
+
+export default About;
