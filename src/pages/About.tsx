65a43a8a69538375664faed6a4b3889838eba035
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-07-16 13:14:42 +0600

    payment integration done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,33 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nihil.
-      Alias quam voluptates blanditiis dolor molestiae, quidem nam velit
-      dignissimos corrupti ad cumque, minima inventore, enim accusantium eius
-      omnis provident?
+    <div className=" bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">
+          About Our Nursery
+        </h1>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="p-4">
+            <p className="text-gray-700 leading-relaxed">
+              We're tree people. We always have been. Our horticultural
+              experience spans 40 years, multiple college degrees and 3
+              generations. Our goal is to provide our customers with the highest
+              quality trees at the best prices as well as passing on valuable
+              information that will not only help your trees succeed, but also
+              make a lasting difference to our environment. After all, this
+              Earth is the only one we've got, so let's give it our all!
+            </p>
+            <p className="mt-4 text-gray-700 leading-relaxed">
+              Integer hendrerit ipsum sit amet ipsum porta, nec fermentum eros
+              elementum. Nam non felis ligula. Suspendisse nec magna vitae nisi
+              consectetur feugiat. Ut efficitur, est eget dignissim posuere, dui
+              leo feugiat lectus, in ultrices dolor mauris nec dui. Aenean
+              accumsan, velit sit amet tempor tincidunt, est tortor fermentum
+              turpis, vel tincidunt est sapien vel libero.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/products/CheckOutForm.tsx
+++ src/pages/products/CheckOutForm.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
+import React, { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import {
   Elements,
@@ -9,14 +9,16 @@ import {
 } from "@stripe/react-stripe-js";
 import { useLocation } from "react-router-dom";
 
-// Load Stripe outside of a component’s render to avoid recreating the Stripe object on every render.
-const stripePromise = loadStripe("your-publishable-key-here");
+const stripePromise = loadStripe(
+  "pk_test_51M6eOGCnxyBmnrrVLNr4LHEpniAkZ1FhXx1cjpSZ5p6yVqQxOgF4HPGbqBs7FhDHHZFSOERMRGoOP61VqNAqa1ix008QRtthlG"
+);
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const location = useLocation();
   const { item, quantity } = location.state || {};
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -33,26 +35,53 @@ const CheckoutForm = () => {
     if (error) {
       console.error(error);
     } else {
-      // Send paymentMethod.id and item data to your backend for further processing
       console.log(paymentMethod, item, quantity);
+      setPaymentSuccess(true); // Set payment success state to true
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <p>Name: {item?.title}</p>
-        <p>Price: ${item?.price * quantity}</p>
-        <p>Quantity: {quantity}</p>
+      <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden md:max-w-2xl m-20 p-10">
+        <div className="md:flex">
+          <div className="w-full p-4">
+            {paymentSuccess && (
+              <div
+                className="bg-green-100 border-l-4 border-green-500 text-green-900 p-4 mb-4"
+                role="alert"
+              >
+                <p className="font-bold">Payment successful!</p>
+                <p>Your payment has been processed.</p>
+              </div>
+            )}
+            <div className="mb-4">
+              <h2 className="text-lg font-semibold">Payment</h2>
+            </div>
+            <div className="mb-4 tex-lg">
+              <p className=" mb-2 text-black">Name: {item?.title}</p>
+              <p className=" mb-2 text-black">
+                Price: ${item?.price * quantity}
+              </p>
+              <p className=" text-black">Quantity: {quantity}</p>
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium text-black mb-2">
+                Card Details
+              </label>
+              <div className="mt-1">
+                <CardElement className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              disabled={!stripe}
+            >
+              Pay Now
+            </button>
+          </div>
+        </div>
       </div>
-      <CardElement />
-      <button
-        type="submit"
-        className="inline-flex items-center justify-center w-full h-10 px-4 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-500 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-        disabled={!stripe}
-      >
-        Pay Now
-      </button>
     </form>
   );
 };
